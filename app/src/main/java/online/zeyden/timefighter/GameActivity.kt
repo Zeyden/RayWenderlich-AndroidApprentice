dfb9f21f0f85b63fd63a9202a7c607c93afd4a72
Azat Shamsullin <zeydendeveloper@gmail.com>
2019-10-09 22:18:47 +0300

    TimeFighter

---
+++ app/src/main/java/online/zeyden/timefighter/GameActivity.kt
@@ -0,0 +1,12 @@
+package online.zeyden.timefighter
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class GameActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game)
+    }
+}
